b923c473deee9b77296cd1baa605d89ffd9b3f64
MonarchRyuzaki <shivamganguly2004@gmail.com>
2026-01-04 19:01:22 +0530

    refactor: improved deserialization of handshake message

--- app/peers.go
+++ app/peers.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"encoding/hex"
 	"fmt"
+	"io"
 	"net"
+	"time"
 )
 
 type Peer struct {
@@ -18,13 +20,13 @@ func (p *Peer) String() string {
 }
 
 func (p *Peer) handleHandshake() {
-	conn, err := net.Dial("tcp", fmt.Sprintf("%v:%v", p.IP, p.Port))
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%v:%v", p.IP, p.Port), 3*time.Second)
 	if err != nil {
 		fmt.Println("Error Connecting:", err)
 		return
 	}
 	defer conn.Close()
-
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
 	message := handshake.handshakeMsg
 
 	_, e := conn.Write(message)
@@ -35,19 +37,32 @@ func (p *Peer) handleHandshake() {
 
 	// fmt.Printf("Sent to server: %v\n", message)
 
-	reader := bufio.NewReader(conn)
-
-	recvMsg := make([]byte, 100)
-	n, err := reader.Read(recvMsg)
-	if err != nil {
-		fmt.Println("Cant Receive Handshake Message: ", err)
+	recvMsg := make([]byte, 68)
+	_, er := io.ReadFull(conn, recvMsg)
+	if er != nil {
+		fmt.Println("Cant Receive Handshake Message: ", er)
 		return
 	}
 
-	p.deserializeHandshakeMessage(recvMsg[:n])
+	p.deserializeHandshakeMessage(recvMsg)
 
 }
 
 func (p *Peer) deserializeHandshakeMessage(b []byte) {
-	p.PeerID = [20]byte(b[len(b)-20:])
+	if len(b) < 68 {
+		fmt.Println("Error: Handshake too short")
+		return
+	}
+
+	if b[0] != 19 || string(b[1:20]) != "BitTorrent protocol" {
+		fmt.Println("Error: Invalid protocol")
+		return
+	}
+
+	if !bytes.Equal(b[28:48], handshake.InfoHash[:]) {
+		fmt.Printf("Error: InfoHash mismatch. Expected %x, Got %x\n", handshake.InfoHash, b[28:48])
+		return
+	}
+
+	copy(p.PeerID[:], b[48:])
 }
